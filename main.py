7f9cdb1299a4c93aa779d399d203944422d5b303
Lusiane Lima <lusianelima5@gmail.com>
2025-10-09 19:16:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from routes_vendas import router as venda_router
+from routes_vendas import router as vendas_router
+from routes_compras import router as compras_router
+from routes_consignacao import router as consignacoes_router
+from routes_estoque import router as estoque_router
 
 app = FastAPI(
     title="API Luar Literário",
@@ -7,21 +10,13 @@ app = FastAPI(
     version="1.0"
 )
 
-app.include_router(venda_router, prefix="/api", tags=["Vendas"])
-
-@app.get("/")
-def home():
-    return {"mensagem": "Bem-vindo à API Luar Literário!"}
-
-from fastapi import FastAPI
-from routes_vendas import router as vendas_router
-from routes_compras import router as compras_router
-from routes_consignacao import router as consignacoes_router
-from routes_estoque import router as estoque_router
-
-app = FastAPI(title="API Luar Literário")
-
+# Inclui todos os roteadores
 app.include_router(vendas_router)
 app.include_router(compras_router)
 app.include_router(consignacoes_router)
 app.include_router(estoque_router)
+
+# Rota Home
+@app.get("/")
+def home():
+    return {"mensagem": "Bem-vindo à API Luar Literário!"}
